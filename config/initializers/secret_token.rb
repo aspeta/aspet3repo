bd2109374c723a0aa2bfd67fef9d0d2c10d0c859
Aspet Amirkhanian <aspeta@yahoo.com>
2014-05-14 21:33:54 +0000

    first PR app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PrApp::Application.config.secret_key_base = 'd53285b998ef0fe796a46d62615a1ec3d58d085f0b17b1c3c109be0f3f8900fa33ae5157ce0927388a3efb22c842d1f5e5df74a1940e3a69e36b22e8e58625e4'
